9cfb759acb44f284834d70105126743470ef4159
tomomi adachi <tomoni1997@gmai.com>
2022-12-23 18:34:58 +0900

    詳細・投稿ボタンの実装、タイトル追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,25 @@
 class PostsController < ApplicationController
    def index
+    @posts = Post.all
    end
     
    def new
+    @post = Post.new
    end    
-end
+
+   def create
+    Post.create(post_params)
+    redirect_to new_post_path
+   end
+
+   def show
+   end
+
+   private
+
+   def post_params
+     params.require(:post).permit(:content)
+   end
+end 
+
+
