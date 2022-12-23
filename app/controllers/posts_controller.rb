674a43b69c580c1526a05ab9cc3a01b9c8cf9430
tomomi adachi <tomoni1997@gmai.com>
2022-12-23 19:06:05 +0900

    バリデーションの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,15 @@ class PostsController < ApplicationController
    end    
 
    def create
-    Post.create(post_params)
-    redirect_to new_post_path
-   end
+     @post = Post.new(post_params)
+     if @post.save
+      # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
+      redirect_to posts_path, notice: "投稿を作成しました！"
+     else
+      # 入力フォームを再描画します。
+      render :new
+     end
+    end 
 
    def show
     @post = Post.find(params[:id])
