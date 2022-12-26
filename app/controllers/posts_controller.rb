d4586f8d6036153624b19336d2f3b92ef0e0e315
tomomi adachi <tomoni1997@gmai.com>
2022-12-26 17:16:50 +0900

    削除ボタン・削除/編集時に表示する確認ダイアログの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+   before_action :set_post, only: [:show, :edit, :update, :destroy]
+
    def index
     @posts = Post.all
    end
@@ -35,10 +37,19 @@ class PostsController < ApplicationController
      end
    end
 
+   def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"投稿を削除しました！"
+   end
+
    private
 
    def post_params
      params.require(:post).permit(:content)
+   end
+
+   def set_post
+    @post = Post.find(params[:id])
    end
 end 
 
